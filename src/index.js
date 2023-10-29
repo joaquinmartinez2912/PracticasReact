4f5acc93481a55eaa35ecdcbc8b267b83ee9e07d
Joaquin Martinez <jo.martinez@itecriocuarto.org.ar>
2023-10-29 13:45:37 -0300

    correcion theme

--- src/index.js
+++ src/index.js
@@ -6,39 +6,39 @@ import App from './App';
 import { ModeProvider } from './contexts/ModeContext';
 import reportWebVitals from './reportWebVitals';
 
-import { BrowserRouter,
-   RouterProvider,
-  createBrowserRouter 
-} from 'react-router-dom';
-import Protegido from './pages/Protegido';
+// import { BrowserRouter,
+//    RouterProvider,
+//   createBrowserRouter 
+// } from 'react-router-dom';
+// import Protegido from './pages/Protegido';
 
-function Contact (){
-  return(
-    <h1> Contacttos </h1>
-  )
-}
+// function Contact (){
+//   return(
+//     <h1> Contacttos </h1>
+//   )
+// }
 
-function ProductsItem (props){
+// function ProductsItem (props){
   
-  return(
-    <h1> Mostrando el producto ...</h1>
-  )
-}
+//   return(
+//     <h1> Mostrando el producto ...</h1>
+//   )
+// }
 
-const router = createBrowserRouter([
-  {
-    path: "/contact",
-    element: <Contact/>,
-  },
-  {
-    path: "/protegido",
-    element: <Protegido/>,
-  },
-  {
-    path: "/p",
-    element: <ProductsItem/>,
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/contact",
+//     element: <Contact/>,
+//   },
+//   {
+//     path: "/protegido",
+//     element: <Protegido/>,
+//   },
+//   {
+//     path: "/p",
+//     element: <ProductsItem/>,
+//   },
+// ]);
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -52,3 +52,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
